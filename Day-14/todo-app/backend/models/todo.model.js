6a179982c3286a5e3d773a01399bcb5be3840824
Tharun <tharun.wasantha@gmail.com>
2025-12-20 15:02:29 +0530

    connected backend to DB

--- Day-14/todo-app/backend/models/todo.model.js
+++ Day-14/todo-app/backend/models/todo.model.js
@@ -1,14 +1,46 @@
-let todos =[]; //{id: 1, text: "Sample TODO", completed: false}
-let id = 1;
+// Keeping the data in memory for simplicity
+// let todos =[]; //{id: 1, text: "Sample TODO", completed: false}
+// let id = 1;
 
-const TodoModel = {
-    getAll: () => {
-        return todos;
+// const TodoModel = {
+//     getAll: () => {
+//         return todos;
+//     },
+//     create: (text) => {
+//         const newTodo = {id: id++,text, completed: false};
+//         todos.push(newTodo);
+//         return newTodo;
+//     },
+// };
+
+// module.exports = TodoModel;
+
+
+//Using MongoDB with Mongoose
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema(
+    {
+         text: { type: String, required: true },
+        completed : { type: Boolean, default: false},
+    }
+);
+
+const Todo = mongoose.model("todo",todoSchema);
+
+const TodoModel ={
+    getAll: async () => {
+        return await Todo.find();
     },
-    create: (text) => {
-        const newTodo = {id: id++,text, completed: false};
-        todos.push(newTodo);
-        return newTodo;
+
+    create: async (text) => {
+        // const newTodo = new Todo({text});
+        // return await newTodo.save();
+
+        return await Todo.create({
+            text: text,
+            completed: false,
+        });
     },
 };
 
--- Day-14/todo-app/backend/server.js
+++ Day-14/todo-app/backend/server.js
@@ -3,11 +3,18 @@ const app = express();
 const cors = require("cors");
 const port = 3000;
 
+const mongoose = require("mongoose");
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/todos", require("./routes/todo.routes"));
 
+//MongoDB Atlas connection
+mongoose
+  .connect("mongodb+srv://todoUser:todo123@todocluster.ny5i7ip.mongodb.net/tododb")
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Could not connect to MongoDB Atlas", err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
